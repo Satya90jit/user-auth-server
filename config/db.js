5b0f0808f38be60240e0c8a60d160ac51deaffe8
Satya90jit <satyajitsahu2002@gmail.com>
2024-11-05 17:59:06 +0530

    small comment

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-// Function to connect to the database
+// Function to connect to the database....
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {});
